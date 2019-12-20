727d2f8a6fccaf8bdb84e5986ed4ffe94c25cce2
DavidODW <dodwei.dev@gmail.com>
2019-12-20 10:14:24 +0800

    Finish day 08 part 2 challenge

--- day08/main.go
+++ day08/main.go
@@ -15,24 +15,65 @@ func main() {
 	sizeOfPixel := wide * tall
 	totalOfLayers := len(pixels) / (sizeOfPixel)
 
-	pixelCount := make(map[string]int)
-	totalOfZeroDigit := math.MaxInt64
+	{
+		fmt.Println("--- Part One ---")
+		pixelCount := make(map[string]int)
+		totalOfZeroDigit := math.MaxInt64
+		for i := 0; i < totalOfLayers; i++ {
+			layer := string(pixels[i*sizeOfPixel : i*sizeOfPixel+sizeOfPixel])
 
-	for i := 0; i < totalOfLayers; i++ {
-		layer := string(pixels[i*sizeOfPixel : i*sizeOfPixel+sizeOfPixel])
+			currentPixelCount := make(map[string]int)
+			for _, pixel := range layer {
+				currentPixelCount[string(pixel)] = currentPixelCount[string(pixel)] + 1
+			}
 
-		currentPixelCount := make(map[string]int)
-		for _, pixel := range layer {
-			currentPixelCount[string(pixel)] = currentPixelCount[string(pixel)] + 1
+			if currentPixelCount["0"] < totalOfZeroDigit {
+				totalOfZeroDigit = currentPixelCount["0"]
+				pixelCount = currentPixelCount
+			}
 		}
-
-		if currentPixelCount["0"] < totalOfZeroDigit {
-			totalOfZeroDigit = currentPixelCount["0"]
-			pixelCount = currentPixelCount
-		}
+		fmt.Println(pixelCount["1"] * pixelCount["2"])
 	}
 
-	fmt.Println(pixelCount["1"] * pixelCount["2"])
+	{
+		fmt.Println("--- Part Two ---")
+		positionDigitGroup := make(map[int][]string)
+		for i := 0; i < totalOfLayers; i++ {
+			layer := string(pixels[i*sizeOfPixel : i*sizeOfPixel+sizeOfPixel])
+
+			for position, pixel := range layer {
+				positionDigitGroup[position] = append(positionDigitGroup[position], string(pixel))
+			}
+		}
+
+		message := ""
+		for i := 0; i < sizeOfPixel; i++ {
+			for _, digit := range positionDigitGroup[i] {
+				if digit != "2" {
+					message += digit
+					break
+				}
+			}
+		}
+
+		for h := 0; h < tall; h++ {
+			for w := 0; w < wide; w++ {
+				printColor(string(message[(h*wide)+w]))
+			}
+			fmt.Println()
+		}
+	}
+}
+
+func printColor(pixel string) {
+	switch pixel {
+	case "0":
+		fmt.Print("█")
+	case "1":
+		fmt.Print(" ")
+	default:
+		fmt.Print("?")
+	}
 }
 
 func readFromFile(filepath string) []string {
