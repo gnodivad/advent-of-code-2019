86713c1083a4b8bf256f4f09a9b371c661bdb64d
DavidODW <dodwei.dev@gmail.com>
2019-12-22 14:46:12 +0800

    Refactor day02 part 2

--- day02/main.go
+++ day02/main.go
@@ -8,6 +8,25 @@ import (
 
 func main() {
 	inputs := utils.ReadNumbers("day02/input.txt")
-	output := intcode.Run(inputs)
-	fmt.Println(output)
+	{
+		fmt.Println("--- Part One ---")
+		mem := make([]int, len(inputs))
+		copy(mem, inputs)
+		output := intcode.Run(mem)
+		fmt.Println(output)
+	}
+	{
+		fmt.Println("--- Part Two ---")
+		for noun := 0; noun < 100; noun++ {
+			for verb := 0; verb < 100; verb++ {
+				mem := make([]int, len(inputs))
+				copy(mem, inputs)
+				mem[1] = noun
+				mem[2] = verb
+				if output := intcode.Run(mem); output == 19690720 {
+					fmt.Println(100*noun + verb)
+				}
+			}
+		}
+	}
 }
